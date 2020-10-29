c9109158119ee4d9886491326778404032a05715
Jack Hill <jackhill2104@gmail.com>
2020-10-29 16:17:56 +1030

    Add test opmode shell

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfiguration.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.configurations;
+
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public abstract class RobotConfiguration {
+}
