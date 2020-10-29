b49845cb6ca812e3d6a5de73ecc8b06ad1aff076
CultVera18 <62174522+CultVera18@users.noreply.github.com>
2020-10-29 16:52:47 +1030

    Update TestOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestOpMode.java
@@ -11,8 +11,7 @@ import com.qualcomm.robotcore.util.Range;
 public class TestOpMode extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor motor = null;
 
     @Override
     public void runOpMode() {
@@ -23,13 +22,11 @@ public class TestOpMode extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        motor  = hardwareMap.get(DcMotor.class, "motor");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        motor.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -38,7 +35,8 @@ public class TestOpMode extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            
+            //joystick value = motor
+            motor.setPower(gamepad1.left_stick_y);
         }
     }
 }
