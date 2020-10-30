b95a80fb257aef853d26f03eb0bd16f7cf395ca4
Jack Hill <jackhill2104@gmail.com>
2020-10-30 16:29:25 +1030

    Update RobotConfiguration.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/RobotConfiguration.java
@@ -5,4 +5,25 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public abstract class RobotConfiguration {
+
+    private Telemetry telemetry;
+
+    abstract protected void init(HardwareMap hardwareMap, Telemetry telemetry);
+
+    protected Telemetry GetTelemetry() { return telemetry; }
+
+    protected void SetTelemetry(Telemetry telemetry) { this.telemetry = telemetry; }
+
+    protected HardwareDevice GetHardwwareOn(String name, Object hardwareDeviceMapping) {
+        HardwareDevice hardwareDevice = null;
+        try {
+            HardwareMap.DeviceMapping<HardwareDevice> deviceMapping = (HardwareMap.DeviceMapping<HardwareDevice>) hardwareDeviceMapping;
+            hardwareDevice = (HardwareDevice) deviceMapping.get(name);
+        }
+        catch (Throwable e) {
+            try {
+                ErrorUtil.handleCatchAllException(e, GetTelemetry());
+            }
+        }
+    }
 }
