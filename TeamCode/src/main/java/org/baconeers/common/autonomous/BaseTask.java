c445de5093f8058b9bd8539c8bb54fd15751d3cf
phalcr004 <43401190+phalcr004@users.noreply.github.com>
2020-11-07 10:44:02 +1030

    Update BaseTask.java

--- TeamCode/src/main/java/org/baconeers/common/autonomous/BaseTask.java
+++ TeamCode/src/main/java/org/baconeers/common/autonomous/BaseTask.java
@@ -2,44 +2,25 @@ package org.baconeers.common.autonomous;
 
 import org.baconeers.common.teleop.BaconOpMode;
 
-public abstract class BaseTask implements Task {
+public abstract class BaseTask {
 
-    protected double time;
     protected boolean isFinished = false;
     protected double startTime = 0;
     protected BaconOpMode opMode;
 
-
     /**
-     * the number of nanoseconds in a second
+     *      The number of nanoseconds in a second
+     *      Use System.nanoTime() to get the current time in nano seconds
      */
+
     public static final double NANOS_IN_SECONDS = 1000000000.0;
 
 
     public BaseTask(BaconOpMode opMode, double time) {
-
-        this.time = time;
         this.opMode = opMode;
 
     }
 
-    public boolean isFinished() {
-        if (isFinished) {
-            return isFinished;
-        }
-
-        if (startTime == 0) {
-            startTime = getCurrentTime();
-        }
-        if (getCurrentTime() > (startTime + time)) {
-            isFinished = true;
-        }
-        return isFinished;
-    }
-
-
-    private double getCurrentTime() {
-        return System.nanoTime() / NANOS_IN_SECONDS;
-    }
-
+    public abstract void run();
+    public abstract void isFinished();
 }
