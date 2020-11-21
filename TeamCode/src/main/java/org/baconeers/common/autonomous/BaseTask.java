e8883b4949515fe1aeb473de4ab6f2245eb65a5d
phalcr04 <43401190+phalcr04@users.noreply.github.com>
2020-11-22 00:12:42 +1030

    Update BaseTask.java

--- TeamCode/src/main/java/org/baconeers/common/autonomous/BaseTask.java
+++ TeamCode/src/main/java/org/baconeers/common/autonomous/BaseTask.java
@@ -3,7 +3,6 @@ package org.baconeers.common.autonomous;
 import org.baconeers.common.teleop.BaconOpMode;
 
 public abstract class BaseTask {
-
     protected BaconOpMode opMode;
 
     /**
@@ -13,9 +12,25 @@ public abstract class BaseTask {
 
     public static final double NANOS_IN_SECONDS = 1000000000.0;
 
+    /**
+     *
+     *      The following constructor can be called using "super". This
+     *      constructor can be used to initialise variables that every
+     *      task requires to run, such as opMode.
+     *
+     */
+
     public BaseTask(BaconOpMode opMode) {
         this.opMode = opMode;
     }
+
+    /**
+     *
+     *      The below methods are necessary for a task to run. To see the
+     *      use of the methods, see already created task. Otherwise, see
+     *      the AutonomousOpModeTemplate for the calling of the methods.
+     *
+     */
 
     public abstract void run();
     public abstract boolean isFinished();
