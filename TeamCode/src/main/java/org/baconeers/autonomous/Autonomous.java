806e1f95aaf7cf20f3b87390a83e62b8f7e417dc
BryceYoung <62359175+BryceYoung@users.noreply.github.com>
2020-11-21 12:50:47 +1030

    Added Autonomous running code

--- TeamCode/src/main/java/org/baconeers/autonomous/Autonomous.java
+++ TeamCode/src/main/java/org/baconeers/autonomous/Autonomous.java
@@ -2,6 +2,7 @@ package org.baconeers.autonomous;
 
 import org.baconeers.common.autonomous.BaseTask;
 import org.baconeers.common.autonomous.tasks.DriveTask;
+import org.baconeers.common.autonomous.tasks.HarvesterTask;
 import org.baconeers.common.teleop.BaconOpMode;
 import org.baconeers.configurations.UltimateGoalConfiguration;
 import org.baconeers.teleop.UltimateGoalOpMode;
@@ -17,20 +18,20 @@ public class Autonomous {
     private ArrayDeque<BaseTask> tasks = new ArrayDeque<BaseTask>();
 
     private int[] integers = new int[2];
+    private boolean currentTaskRunning = false;
+    private BaseTask task
 
 
     private void activeLoop() {
-
+        tasks.add(new HarvesterTask(opmode,config,true,0))
         tasks.add(new DriveTask(opmode, config, DriveTask.DriveSpeed.MEDIUM, 19));
         tasks.add(new DriveTask(opmode, config, DriveTask.DriveSpeed.SLOW, 1));
         tasks.add(new DriveTask(opmode, config, DriveTask.DriveSpeed.FAST, 23));
 
-
-
-        BaseTask task = tasks.peekFirst();
-
-        task.run();
-
+        if (!currentTaskRunning) {
+            task = tasks.peekFirst();
+            task.run();
+        }
         if(task.isFinished()) {
             tasks.removeFirst();
         }
